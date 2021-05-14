bd86b6ba1c3c3f10e20ef0e4a8c468ed61585c28
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-05-13 22:03:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import time
-import random
 
 repeats = int(input("[?] How many times do you want to send the message?"))
 cooldown = int(input("[?] How many interval do you want between each message?"))
